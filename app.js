86e6f9edfe92d9109c6e6c2af042f3c382e5cf5b
OnTai To <122745906+EvaSuper0115@users.noreply.github.com>
2024-04-17 13:17:45 +0100

    setting a date form
    need to :
    -only select future date
    -only appear when the rentButton is clicked

---
+++ app.js
@@ -0,0 +1,6 @@
+//click date button to select date
+const rentButton = document.querySelector(".rentButton");
+function displayDate() {
+  const dateForm = document.querySelector(".dateForm");
+}
+rentButton.addEventListener("click", displayDate());
