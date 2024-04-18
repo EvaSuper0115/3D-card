fc71529afcb2001464789fc471affdd67058403c
OnTai To <122745906+EvaSuper0115@users.noreply.github.com>
2024-04-18 12:17:29 +0100

    can only select 3 days later and 90 days max

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//can only select date from 3 days later
+//can only select date from 3 days later and 90 days max
 //get today's date
 const today = new Date();
 //get 3 days later by initialising today's date and modify it
@@ -7,8 +7,14 @@ threeDaysLater.setDate(today.getDate() + 3);
 function getThreeDayslater() {
   return threeDaysLater.toISOString().split("T")[0];
 }
+//get 90 days later
+const ninetyDaysLater = new Date(today);
+ninetyDaysLater.setDate(today.getDate() + 90);
+function getNinetyDayslater() {
+  return ninetyDaysLater.toISOString().split("T")[0];
+}
 document.getElementById("dateInput").setAttribute("min", getThreeDayslater());
-
+document.getElementById("dateInput").setAttribute("max", getNinetyDayslater());
 //click date form to select date
 const selectDate = document.querySelector(".dateForm");
 function submitDate() {}
