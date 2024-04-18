306bb05ee0f348250ecff23564a4de75a0773e8b
OnTai To <122745906+EvaSuper0115@users.noreply.github.com>
2024-04-18 12:11:11 +0100

    can only select 3 days later

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
-//can only select future date
+//can only select date from 3 days later
+//get today's date
+const today = new Date();
+//get 3 days later by initialising today's date and modify it
+const threeDaysLater = new Date(today);
+threeDaysLater.setDate(today.getDate() + 3);
+function getThreeDayslater() {
+  return threeDaysLater.toISOString().split("T")[0];
+}
+document.getElementById("dateInput").setAttribute("min", getThreeDayslater());
 
 //click date form to select date
 const selectDate = document.querySelector(".dateForm");
